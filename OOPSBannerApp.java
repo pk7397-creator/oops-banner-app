4a1e12e950ff8867e744266219d49c02aa1900d8
pranavi <pranavi@pranavis-MacBook-Air.local>
2026-03-18 00:11:09 +0530

    UC1: Display Hello World in console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,59 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
-    // Static Inner Class
-    static class CharacterPatternMap {
-
-        char character;
-        String[] pattern;
-
-        CharacterPatternMap(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
-
-        String[] getPattern() {
-            return pattern;
-        }
-    }
-
-    public static void main(String[] args) {
-
-        CharacterPatternMap O = new CharacterPatternMap('O', new String[]{
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        });
-
-        CharacterPatternMap P = new CharacterPatternMap('P', new String[]{
-                "***** ",
-                "*    *",
-                "*    *",
-                "***** ",
-                "*     ",
-                "*     ",
-                "*     "
-        });
-
-        CharacterPatternMap S = new CharacterPatternMap('S', new String[]{
-                " *****",
-                "*     ",
-                "*     ",
-                " *****",
-                "     *",
-                "     *",
-                "***** "
-        });
-
-        String[] o = O.getPattern();
-        String[] p = P.getPattern();
-        String[] s = S.getPattern();
-
-        for (int i = 0; i < o.length; i++) {
-            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
-        }
-    }
-}
+    static Map
