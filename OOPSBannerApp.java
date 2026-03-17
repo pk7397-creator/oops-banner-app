cdb2f97b6488cb1ac152c56011c6de4ce8e0d960
pranavi <pranavi@pranavis-MacBook-Air.local>
2026-03-18 00:16:40 +0530

    UC2: Display Hello with command-line argument

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class OOPSBannerApp {
 
-    static Map
+    public static void main(String[] args) {
+
+        if(args.length > 0){
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello");
+        }
+
+    }
+
+}
