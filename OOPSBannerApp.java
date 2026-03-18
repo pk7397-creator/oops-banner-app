7998727619ca3515f5b3de9dfc670afc8915e6a7
pranavi <pranavi@pranavis-MacBook-Air.local>
2026-03-18 13:33:37 +0530

    UC5: Display Hello using enhanced for loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,19 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // If no names are provided
+        // If no arguments are given
         if (args.length == 0) {
-            System.out.println("Hello World");
-        } 
+            System.out.println("Hello, World!");
+        }
         else {
 
-            // Loop through all command line arguments
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello " + args[i]);
+            // Enhanced for loop to process names
+            for (String name : args) {
+                System.out.println("Hello " + name);
             }
 
         }
+
     }
 
 }
